e6961a0bba746ca3ce92914c7afa517c29f51ad0
gminova <minova1995@gmail.com>
2019-06-06 13:02:38 +0100

    mobile nav button

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//document.onload(alert('script loaded')) 
+var mobileNav = document.getElementById("mobileNav");
+var sideNav = document.getElementById("sideNav");
+var closeNav = document.getElementById("closeNav");
+
+mobileNav.onclick = function toggleNav() {
+    mobileNav.style.display = "none";
+    sideNav.style.display = "block";
+}
+
+closeNav.onclick = function closeNav() {
+    mobileNav.style.display = "block";
+    sideNav.style.display = "none";
+}
